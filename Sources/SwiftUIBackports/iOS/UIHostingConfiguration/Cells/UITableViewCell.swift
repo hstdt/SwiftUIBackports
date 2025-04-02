03d32d455a52ddabeb630c5747b91040f46c4260
hstdt <hstdt@users.noreply.github.com>
2025-04-02 11:15:19 +0800

    try fix UICollectionViewCell crash

--- Sources/SwiftUIBackports/iOS/UIHostingConfiguration/Cells/UITableViewCell.swift
+++ Sources/SwiftUIBackports/iOS/UIHostingConfiguration/Cells/UITableViewCell.swift
@@ -6,11 +6,11 @@ import ObjectiveC
 
 extension UITableViewCell {
     
-    private static var configuredViewAssociatedKey: Void?
-    
+    nonisolated(unsafe) static var configuredViewAssociatedKey = malloc(1)
+
     fileprivate var configuredView: UIView? {
         get { objc_getAssociatedObject(self, &Self.configuredViewAssociatedKey) as? UIView }
-        set { objc_setAssociatedObject(self, &Self.configuredViewAssociatedKey, newValue, .OBJC_ASSOCIATION_ASSIGN) }
+        set { objc_setAssociatedObject(self, &Self.configuredViewAssociatedKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
     }
 }
 
